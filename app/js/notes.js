e29685227ab44f1d8520f19006e87a3da2a819f5
AlexanderDong09 <alexanderdong09@gmail.com>
2024-11-19 12:32:39 -0500

    vasda

--- app/js/notes.js
+++ app/js/notes.js
@@ -32,9 +32,26 @@ async function getData() {
 
   const data = await response.json();
 
-  console.log(response); // if something breaks change to response
+  console.log(data); // if something breaks change to response
+  createCards(data);
 
   // make the crap appear on the screen later
 }
 
 getData();
+
+function createCards(data) {
+  data.forEach((item) => {
+    const card = `
+      <div class="card">
+          <h2 class="header">${item.title}</h2>
+          <img src="${item.hdurl}">
+          <h3>Was APOD on: ${item.date}</h3>
+          <h4>Copyright: ${item.copyright}</h4>
+          <h5>Image Description: ${item.explanation}</h5>
+      </div>
+    `;
+
+    DOMSelectors.container.insertAdjacentHTML("beforeend", card);
+  });
+}
